49fa32fe7197478ddca20d16509f877d6e5e7b36
Heidi4 <erickkaloki28@gmail.com>
2024-03-05 15:38:18 +0300

    Handling multiple input and use case of (...) spread operator

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState({ firstName: "", lastName: "" });
 
   function handleChange(e) {
     setName(e.target.value);
   }
+
   return (
     <div>
       <form>
+        {name.firstName} - {name.lastName}
+        <p></p>
         <input
           // onChange={(e) => handleChange(e)}
-          onChange={(e) => setName(e.target.value)}
           type="text"
-          value={name}
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
+          value={name.firstName}
+          placeholder=" Firstname"
+        />
+        <input
+          type="text"
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+          value={name.lastName}
+          placeholder="Lastname"
         />
       </form>
     </div>
