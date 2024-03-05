afb58d1430566427ce0a9fc16b54dcf646256ec0
Heidi4 <erickkaloki28@gmail.com>
2024-03-05 12:36:17 +0300

    Passing Arrays and objects to props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Hello from "./components/Hello";
 
 function App() {
+  const seatNumbers = [1, 4, 7];
+  const person = {
+    name: "Eric",
+    message: "Hi there",
+    emoji: "🙋‍♀️",
+    seatNumbers: [1, 4, 7],
+  };
   return (
     <div className="App">
-      <Hello name="Eric" message="Hi there" emoji="🙋‍♀️"/>
-      
+      <Hello
+        person={person}
+      />
     </div>
   );
 }
