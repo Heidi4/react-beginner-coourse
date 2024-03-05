69b559d0ff46132e078fefb0f87f1886dfa12995
Heidi4 <erickkaloki28@gmail.com>
2024-03-05 13:26:54 +0300

    Rendering complex array with object

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 
 function Fruits() {
-  const fruits = ["Apple", "Pears", "Pineaple", "Mango", "Banana"];
+  //   const fruits = ["Apple", "Pears", "Pineaple", "Mango", "Banana"];
+  const fruits = [
+    {
+      name: "Apple",
+      price: 10,
+      emoji: "🍎",
+    },
+    {
+      name: "Mango",
+      price: 5,
+      emoji: "🥭",
+    },
+    {
+      name: "Banana",
+      price: 2,
+      emoji: "🍌",
+    },
+    {
+      name: "Orange",
+      price: 2,
+      emoji: "🍊",
+    },
+    {
+      name: "Pears",
+      price: 2,
+      emoji: "🍐",
+    },
+  ];
   return (
     <div>
       <ul>
-        {fruits.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
-        ))}
+        {fruits.map(fruit => <li key={fruit.name}>{fruit.emoji} {fruit.name} ${fruit.price}</li>)}
       </ul>
     </div>
   );
