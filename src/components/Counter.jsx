dd958fa5347a87c7513359060f5a6376a2d9897d
Heidi4 <erickkaloki28@gmail.com>
2024-03-05 14:50:23 +0300

    Working with multiple states that are in sync

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 
 function Counter() {
+  // js
   const [count, setCount] = useState(0);
+  const [incrementBy, setIncrementBy] = useState(2);
+
   function increment() {
-    setCount(count + 1);
+    setCount(count + incrementBy);
   }
   function decrement() {
-    setCount(count - 1);
+    setCount(count - incrementBy);
   }
+  function increaseIncrement() {
+    setIncrementBy(incrementBy + 1);
+  }
+  function decreaseIncrement() {
+    setIncrementBy(incrementBy - 1);
+  }
+  // jsx
   return (
     <div>
       <h1>Count value is: {count}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+
+      <h1>We are incrementing by: {incrementBy}</h1>
+      <button onClick={increaseIncrement}>Increase Increment</button>
+      <button onClick={decreaseIncrement}>Decrease Increment</button>
     </div>
   );
 }
