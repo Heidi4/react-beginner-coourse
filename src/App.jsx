9ccaf1ee9550a4f67746a54c51b79b63f908e100
Heidi4 <erickkaloki28@gmail.com>
2024-03-05 12:16:37 +0300

    Maiking simple components dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Hello from "./components/Hello";
 
 function App() {
-  return <div className="App">
-    <Hello/>
-    </div>;
+  return (
+    <div className="App">
+      <Hello name="Eric" message="Hi there"/>
+      <Hello name="Mike" message="Good there"/>
+      <Hello name="Josh" message="Hi there"/>
+      <Hello name="Waiter"/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,6 +1,11 @@
-function Hello() {
-  return <h1>Hello from a component</h1>;
-}
+function Hello(props) {
 
+    console.log(props)
+  return (
+    <div>
+      <h1>{props.message} {props.name}</h1>
+    </div>
+  );
+}
 
 export default Hello;
